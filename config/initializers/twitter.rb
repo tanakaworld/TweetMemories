54ad43901855a51ed48f6d4aa8c903c10b277199
yutaro <y.tanaka.kcbbf@gmail.com>
2014-02-16 18:27:20 +0900

    Twitter設定を追加
    figaroの定数文字列も変更

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = ENV["TW_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TW_CONSUMER_SECRET"]
+  config.oauth_token = ENV["TW_ACCESS_TOKEN"]
+  config.oauth_token_secret = ENV["TW_ACCESS_TOKEN_SECRET"]
+end
